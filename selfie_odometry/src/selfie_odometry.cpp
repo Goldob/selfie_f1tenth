46a7a60a8142a3b53142dca29c38ff5f1b1a5c14
Mateusz Szczygielski <mat.szczygielski1@gmail.com>
2018-09-16 15:09:55 +0200

    Odometry using distance data from stm

--- selfie_odometry/src/selfie_odometry.cpp
+++ selfie_odometry/src/selfie_odometry.cpp
@@ -4,24 +4,73 @@
 #include <std_msgs/Float32.h>
 #include <sensor_msgs/Imu.h>
 
-float speed=0.0;
+float speed = 0;
 
-double roll,pitch,yaw=0.0;
+double roll = 0;
+double pitch = 0;
+double yaw = 0;
 
-double x = 0.0;
-double y = 0.0;
-//double th = 0.0;
+float x = 0;
+float y = 0;
 
-double vx = 0.0;
-double vy = 0.0;
-double vth = 0.0;
+float vx = 0;
+float vy = 0;
+float vyaw = 0;
 
-void speedCallback(const std_msgs::Float32 &msg){
-  speed=msg.data;
+float currentDistance = 0;
+float lastDistance = 0;
+float deltaDistance = 0;
+float dt = 0;
+float dx = 0;
+float dy = 0;
+
+ros::Time current_time, last_time;
+
+ros::Publisher odom_pub;
+geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(yaw);
+
+void distanceCallback(const std_msgs::Float32 &msg){
+  current_time = ros::Time::now();
+  lastDistance = currentDistance;
+  currentDistance = msg.data / 1000;
+  deltaDistance = currentDistance - lastDistance;
+  dt = (current_time - last_time).toSec();
+  speed = deltaDistance / dt;
+  vx = speed * cos(yaw);
+  vy = speed * sin(yaw);
+  
+  dx = deltaDistance * cos(yaw);
+  dy = deltaDistance * sin(yaw);
+
+  x += dx;
+  y += dy;
+
+  //publish the odometry message over ROS
+  nav_msgs::Odometry odom;
+  odom.header.stamp = current_time;
+  odom.header.frame_id = "base_frame";
+
+  //set the position
+  odom.pose.pose.position.x = x;
+  odom.pose.pose.position.y = y;
+  odom.pose.pose.position.z = 0.0;
+  odom.pose.pose.orientation = odom_quat;
+
+  //set the velocity
+  odom.child_frame_id = "rear_axis_frame";
+  odom.twist.twist.linear.x = vx;
+  odom.twist.twist.linear.y = vy;
+  odom.twist.twist.angular.z = vyaw;
+  
+  //publish the message
+  odom_pub.publish(odom);
+
+  last_time = current_time;
 }
 
 void imuCallback(const sensor_msgs::Imu &msg){
-  vth=msg.angular_velocity.z;
+
+  vyaw = msg.angular_velocity.z;
 
   tf::Quaternion q(
   msg.orientation.x,
@@ -30,46 +79,31 @@ void imuCallback(const sensor_msgs::Imu &msg){
   msg.orientation.w);
 
   tf::Matrix3x3 m(q);
-  m.getRPY(roll,pitch,yaw);
-
-  vx=speed*cos(yaw);
-  vy=speed*sin(yaw);
+  m.getRPY(yaw,pitch,roll);
+  //quaternion created from yaw
+  odom_quat = tf::createQuaternionMsgFromYaw(yaw);
 }
-
 
 int main(int argc, char** argv){
 
-
   ros::init(argc, argv, "selfie_odometry");
+  
   ros::NodeHandle n;
   
-  ros::Subscriber sub_speed=n.subscribe("speed", 50, speedCallback);
+  odom_pub = n.advertise<nav_msgs::Odometry>("odom", 50);
+  ros::Subscriber sub_distance=n.subscribe("distance", 50, distanceCallback);
   ros::Subscriber sub_imu=n.subscribe("imu", 50, imuCallback);
 
-  ros::Publisher odom_pub = n.advertise<nav_msgs::Odometry>("odom", 50);
   tf::TransformBroadcaster odom_broadcaster;
 
-  ros::Time current_time, last_time;
   current_time = ros::Time::now();
   last_time = ros::Time::now();
+
+  ros::Rate loop_rate(10);
 
   while(n.ok()){
 
     ros::spinOnce();               // check for incoming messages
-    current_time = ros::Time::now();
-
-    //compute odometry in a typical way given the velocities of the robot
-    double dt = (current_time - last_time).toSec();
-    double delta_x = vx * dt;
-    double delta_y = vy * dt;
-    //double delta_th = vth * dt;
-
-    x += delta_x;
-    y += delta_y;
-    //th += delta_th;
-
-    //quaternion created from yaw
-    geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(yaw);
 
     //publish the transform over tf
     geometry_msgs::TransformStamped odom_trans;
@@ -85,26 +119,6 @@ int main(int argc, char** argv){
     //send the transform
     odom_broadcaster.sendTransform(odom_trans);
 
-    //publish the odometry message over ROS
-    nav_msgs::Odometry odom;
-    odom.header.stamp = current_time;
-    odom.header.frame_id = "base_frame";
-
-    //set the position
-    odom.pose.pose.position.x = x;
-    odom.pose.pose.position.y = y;
-    odom.pose.pose.position.z = 0.0;
-    odom.pose.pose.orientation = odom_quat;
-
-    //set the velocity
-    odom.child_frame_id = "rear_axis_frame";
-    odom.twist.twist.linear.x = vx;
-    odom.twist.twist.linear.y = vy;
-    odom.twist.twist.angular.z = vth;
-
-    //publish the message
-    odom_pub.publish(odom);
-
-    last_time = current_time;
+    loop_rate.sleep();
   }
 }
