75f8cebb44880a60b7de80ab4bb15a86e01195b0
Adam Gotlib <gotlib.adam+dev@gmail.com>
2018-09-17 20:18:53 +0200

    Refactor odometry code, add initalization for yaw & distance

--- selfie_odometry/src/selfie_odometry.cpp
+++ selfie_odometry/src/selfie_odometry.cpp
@@ -4,11 +4,16 @@
 #include <std_msgs/Float32.h>
 #include <sensor_msgs/Imu.h>
 
+#define ODOM_FRAME "odom"
+
 double speed = 0;
 
 double roll = 0;
 double pitch = 0;
 double yaw = 0;
+
+double base_yaw = 0;
+bool yaw_initialized = false;
 
 double x = 0;
 double y = 0;
@@ -24,7 +29,8 @@ double dt = 0;
 double dx = 0;
 double dy = 0;
 
-ros::Time current_time, last_time;
+ros::Time current_time, last_distance_update;
+bool distance_initialized = false;
 
 ros::Publisher odom_pub;
 geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(yaw);
@@ -33,45 +39,31 @@ std::string rear_axis_frame;
 
 void distanceCallback(const std_msgs::Float32 &msg)
 {
+
   current_time = ros::Time::now();
-  lastDistance = currentDistance;
   currentDistance = msg.data / 1000;
-  deltaDistance = currentDistance - lastDistance;
-  dt = (current_time - last_time).toSec();
-  if (dt != 0)
+
+  if (distance_initialized)
   {
-    speed = deltaDistance / dt;
-    vx = speed * cos(yaw);
-    vy = speed * sin(yaw);
+    deltaDistance = currentDistance - lastDistance;
+    dt = (current_time - last_distance_update).toSec();
+    if (dt != 0)
+    {
+      speed = deltaDistance / dt;
+      vx = speed * cos(yaw);
+      vy = speed * sin(yaw);
+    }
+
+    dx = deltaDistance * cos(yaw);
+    dy = deltaDistance * sin(yaw);
+
+    x += dx;
+    y += dy;
   }
 
-  dx = deltaDistance * cos(yaw);
-  dy = deltaDistance * sin(yaw);
-
-  x += dx;
-  y += dy;
-
-  // publish the odometry message over ROS
-  nav_msgs::Odometry odom;
-  odom.header.stamp = current_time;
-  odom.header.frame_id = "odom";
-
-  // set the position
-  odom.pose.pose.position.x = x;
-  odom.pose.pose.position.y = y;
-  odom.pose.pose.position.z = 0.0;
-  odom.pose.pose.orientation = odom_quat;
-
-  // set the velocity
-  odom.child_frame_id = rear_axis_frame;
-  odom.twist.twist.linear.x = vx;
-  odom.twist.twist.linear.y = vy;
-  odom.twist.twist.angular.z = vyaw;
-
-  // publish the message
-  odom_pub.publish(odom);
-
-  last_time = current_time;
+  distance_initialized = true;
+  last_distance_update = current_time;
+  lastDistance = currentDistance;
 }
 
 void imuCallback(const sensor_msgs::Imu &msg)
@@ -87,6 +79,13 @@ void imuCallback(const sensor_msgs::Imu &msg)
 
   tf::Matrix3x3 m(q);
   m.getRPY(roll, pitch, yaw);
+
+  if (!yaw_initialized) {
+    base_yaw = yaw;
+    yaw_initialized = true;
+  }
+
+  yaw -= base_yaw;
 
   // quaternion created from yaw
   odom_quat = tf::createQuaternionMsgFromYaw(yaw);
@@ -106,9 +105,6 @@ int main(int argc, char** argv)
   rear_axis_frame = n.param<std::string>("rear_axis_frame", "base_link");
   tf::TransformBroadcaster odom_broadcaster;
 
-  current_time = ros::Time::now();
-  last_time = ros::Time::now();
-
   ros::Rate loop_rate(10);
 
   while (n.ok())
@@ -116,11 +112,12 @@ int main(int argc, char** argv)
 
     // check for incoming messages
     ros::spinOnce();
+    current_time = ros::Time::now();
 
     // publish the transform over tf
     geometry_msgs::TransformStamped odom_trans;
     odom_trans.header.stamp = current_time;
-    odom_trans.header.frame_id = "odom";
+    odom_trans.header.frame_id = ODOM_FRAME;
     odom_trans.child_frame_id = rear_axis_frame;
 
     odom_trans.transform.translation.x = x;
@@ -130,6 +127,26 @@ int main(int argc, char** argv)
 
     // send the transform
     odom_broadcaster.sendTransform(odom_trans);
+
+    // publish the odometry message over ROS
+    nav_msgs::Odometry odom;
+    odom.header.stamp = current_time;
+    odom.header.frame_id = ODOM_FRAME;
+
+    // set the position
+    odom.pose.pose.position.x = x;
+    odom.pose.pose.position.y = y;
+    odom.pose.pose.position.z = 0.0;
+    odom.pose.pose.orientation = odom_quat;
+
+    // set the velocity
+    odom.child_frame_id = rear_axis_frame;
+    odom.twist.twist.linear.x = vx;
+    odom.twist.twist.linear.y = vy;
+    odom.twist.twist.angular.z = vyaw;
+
+    // publish the message
+    odom_pub.publish(odom);
 
     loop_rate.sleep();
   }
